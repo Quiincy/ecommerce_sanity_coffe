4ebf3411247def28d215eeefe54518c727c65473
Quiincy <mishabelous11@gmail.com>
2023-04-24 20:16:19 +0300

    updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import { AiFillInstagram, AiFillFacebook } from 'react-icons/ai'
 const Footer = () => {
     return (
         <div className='footer-container'>
-            <p>2022 Broadway Coffee All rights reserved</p>
+            <p>2023 Broadway Coffee All rights reserved</p>
             <p className='icons'>
                 <a href="https://www.instagram.com/coffee_broadway_/" target="_blank" rel="noreferrer"><AiFillInstagram /></a>
                 <a href="https://www.facebook.com/BroadwayCoffeeVerbitskogo/" target="_blank" rel="noreferrer"><AiFillFacebook /></a>
